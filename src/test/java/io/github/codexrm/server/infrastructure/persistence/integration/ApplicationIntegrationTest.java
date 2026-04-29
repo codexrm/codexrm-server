320a852c6f21326a94d1c611827638e38994bb76
Marynes <marynes0111@gmail.com>
2026-04-29 09:42:17 -0300

    test(integration): add Testcontainers setup and e2e tests scaffold

---
+++ src/test/java/io/github/codexrm/server/infrastructure/persistence/integration/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package io.github.codexrm.server.infrastructure.persistence.integration;
+
+import org.junit.jupiter.api.Test;
+
+class ApplicationIntegrationTest extends BaseIntegrationTest {
+
+    @Test
+    void contextLoads() {
+    }
+}
